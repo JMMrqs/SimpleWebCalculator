0bbbe1a2d0f12926907a8df271e4358354981ab0
João M. Mrqs <joao.marcelo.mm@gmail.com>
2023-08-03 01:46:58 -0300

    first commit, html/css first version done

---
+++ index.js
@@ -0,0 +1,6 @@
+// addEventListener(onclick, () => {
+
+// })
+
+// I should probably apply mudular event listeners that singularly change what each button press does.
+//It might make the code longer, but the way that the HTML is setup, the code might end up slower due to too many comparisons, so modularity seems like a better approach.
