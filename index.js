fefd54830cd757e9438717ecb5ce964c4c02d99d
João M. Mrqs <joao.marcelo.mm@gmail.com>
2023-08-03 14:48:19 -0300

    formatted code

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-const screen = document.querySelector('#screen');
-const digits = document.querySelectorAll('.digit');
-let baseNumber = '';
-let buffNumber = '';
-let operation = '';
+const screen = document.querySelector("#screen");
+const digits = document.querySelectorAll(".digit");
+let baseNumber = "";
+let buffNumber = "";
+let operation = "";
 const operator = {
-    add: 'add',
-    subtract: 'subtract',
-    multiply: 'multiply',
-    divide: 'divide'
-}
-digits.forEach(digitInput => {
-    digitInput.addEventListener("click", ()=> {
+    add: "add",
+    subtract: "subtract",
+    multiply: "multiply",
+    divide: "divide",
+};
+digits.forEach((digitInput) => {
+    digitInput.addEventListener("click", () => {
         buffNumber += digitInput.value;
         screen.innerHTML = buffNumber;
-    })
+    });
 });
 
-document.getElementById('add').addEventListener('click', ()=> {
+document.getElementById("add").addEventListener("click", () => {
     if (baseNumber == 0) {
         baseNumber = buffNumber.slice();
     }
-    buffNumber = '';
+    buffNumber = "";
     operation = operator.add;
     screen.innerHTML = baseNumber;
 });
-document.getElementById('subtract').addEventListener('click', ()=> {
+document.getElementById("subtract").addEventListener("click", () => {
     if (baseNumber == 0) {
         baseNumber = buffNumber.slice();
     }
-    buffNumber = '';
+    buffNumber = "";
     operation = operator.subtract;
     screen.innerHTML = baseNumber;
 });
-document.getElementById('multiply').addEventListener('click', ()=> {
+document.getElementById("multiply").addEventListener("click", () => {
     if (baseNumber == 0) {
         baseNumber = buffNumber.slice();
     }
-    buffNumber = '';
+    buffNumber = "";
     operation = operator.multiply;
     screen.innerHTML = baseNumber;
 });
-document.getElementById('divide').addEventListener('click', ()=> {
+document.getElementById("divide").addEventListener("click", () => {
     if (baseNumber == 0) {
         baseNumber = buffNumber.slice();
     }
-    buffNumber = '';
+    buffNumber = "";
     operation = operator.divide;
     screen.innerHTML = baseNumber;
 });
 
-document.getElementById('equal').addEventListener('click', ()=> {
+document.getElementById("equal").addEventListener("click", () => {
     let base = Number(baseNumber);
     let buff = Number(buffNumber);
     let orderCount = 0;
@@ -59,33 +59,37 @@ document.getElementById('equal').addEventListener('click', ()=> {
         orderCount++;
     }
     switch (operation) {
-        case operator.add: baseNumber = base + buff;
+        case operator.add:
+            baseNumber = base + buff;
             break;
-        case operator.subtract: baseNumber = base - buff;
+        case operator.subtract:
+            baseNumber = base - buff;
             break;
-        case operator.multiply: baseNumber = base * buff;
+        case operator.multiply:
+            baseNumber = base * buff;
             break;
-        case operator.divide: baseNumber = base / buff;
+        case operator.divide:
+            baseNumber = base / buff;
             break;
     }
     if (orderCount != 0) {
-        baseNumber /= (10 * orderCount);
+        baseNumber /= 10 * orderCount;
     }
-    buffNumber = '';
+    buffNumber = "";
     screen.innerHTML = baseNumber;
 });
 
-document.getElementById('clear').addEventListener('click', ()=> {
-    baseNumber = ''
-    buffNumber = '';
+document.getElementById("clear").addEventListener("click", () => {
+    baseNumber = "";
+    buffNumber = "";
     operation = null;
     screen.innerHTML = buffNumber;
 });
 
-document.getElementById('erase').addEventListener('click', ()=> {
+document.getElementById("erase").addEventListener("click", () => {
     if (buffNumber == 0) {
-        baseNumber = ''
-        buffNumber = '';
+        baseNumber = "";
+        buffNumber = "";
         operation = null;
         screen.innerHTML = buffNumber;
         return;
